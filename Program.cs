5c50db48275c962d0d1f3775c20aad3f27b2426e
Ayotola Jinadu <cse080123@gmail.com>
2022-02-12 10:35:51 +0100

    First yewande and kevin project class

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WandeKevin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
