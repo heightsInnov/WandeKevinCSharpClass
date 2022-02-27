8ff14fc9d56ed18e6a0041c0cc61675dde1a466c
kevin-omini <kevinomini@gmail.com>
2022-02-27 12:54:08 +0100

    To Console Output to a File left in Assignment

--- Program.cs
+++ Program.cs
@@ -6,36 +6,37 @@ namespace WandeKevin
 {
     class Program
     {
-        /*        public static void Mains(string[] args)
-                {
-                    Console.WriteLine("================= School Details =========================");
-                    School futa = new School("Federal University of Technology, Akure", "Akure, Nigeria", 4);
-                    School uniport = new School("University of Port Harcourt", "https://www.uniport.edu.ng");
-                    School unilag = new School("University of  Lagos", "Lagos, Nigeria", "Public");
-                    Console.WriteLine(futa.Print() + "\n" + "\n" + uniport.Print() + "\n" + "\n" + unilag.Print());
-                    Console.WriteLine("===========================================================" + "\n");
+        // School, Course, and Student
+/*        public static void Main(string[] args)
+        {
+            Console.WriteLine("================= School Details =========================");
+            School futa = new School("Federal University of Technology, Akure", "Akure, Nigeria", 4);
+            School uniport = new School("University of Port Harcourt", "https://www.uniport.edu.ng");
+            School unilag = new School("University of  Lagos", "Lagos, Nigeria", "Public");
+            Console.WriteLine(futa.Print() + "\n" + "\n" + uniport.Print() + "\n" + "\n" + unilag.Print());
+            Console.WriteLine("===========================================================" + "\n");
 
+            
+            Console.WriteLine("================= Course Details =========================");
+            Course calculus = new Course("MTH 120", "University of  Lagos");
+            Course organicChemistry = new Course("CHM 130", 3, "Second Semester", "University of  Port Harcourt");
+            Course cSharp = new Course("CSC 101", "Introduction to Programming in C#", new string[] { "Ayotola Jinadu", "Yewande Akinjewe" }, "University of  Lagos");
+            Console.WriteLine(calculus.Print() + "\n" + "\n" + organicChemistry.Print() + "\n" + "\n" + cSharp.Print());
+            Console.WriteLine("===========================================================" + "\n");
+            
 
+            Console.WriteLine("================= Student Details =========================");
+            Student tola = new Student("U2019/2035001");
+            Student kevin = new Student("Kevin", "Omini", "kevinomini@gmail.com");
+            Student yewande = new Student("U2022/1015007", "Computer Science", 'M', true);
+            Console.WriteLine(tola.Print() + "\n" + "\n" + kevin.Print() + "\n" + "\n" + yewande.Print());
+            Console.WriteLine("===========================================================" + "\n");
 
-                    Console.WriteLine("================= Course Details =========================");
-                    Course calculus = new Course("MTH 120", "University of  Lagos");
-                    Course organicChemistry = new Course("CHM 130", 3, "Second Semester", "University of  Port Harcourt");
-                    Course cSharp = new Course("CSC 101", "Introduction to Programming in C#", new string[] { "Ayotola Jinadu", "Yewande Akinjewe" }, "University of  Lagos");
-                    Console.WriteLine(calculus.Print() + "\n" + "\n" + organicChemistry.Print() + "\n" + "\n" + cSharp.Print());
-                    Console.WriteLine("===========================================================" + "\n");
+        }
+*/
 
-
-
-                    Console.WriteLine("================= Student Details =========================");
-                    Student tola = new Student("U2019/2035001");
-                    Student kevin = new Student("Kevin", "Omini", "kevinomini@gmail.com");
-                    Student yewande = new Student("U2022/1015007", "Computer Science", 'M', true);
-                    Console.WriteLine(tola.Print() + "\n" + "\n" + kevin.Print() + "\n" + "\n" + yewande.Print());
-                    Console.WriteLine("===========================================================" + "\n");
-
-                }*/
-
-        public static void Main(string[] args)
+        // Table
+        /*public static void Main(string[] args)
         {
             Console.WriteLine("================= Table Description =========================");
             Table table = new Table("Black Horse Power", "blue", true);
@@ -44,11 +45,10 @@ namespace WandeKevin
             Console.WriteLine(table.ToString() + "\n" + "\n" + table1.ToString() + "\n" + "\n" + table2.ToString());
             Console.WriteLine("===========================================================" + "\n");
 
-
-
         }
+*/
 
-
+        // Calculator
         /*        public static void Main(string[] args)
                 {
                     Console.WriteLine("=============== Hello Calculator! ===============" + "\n");
