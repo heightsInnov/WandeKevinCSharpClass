9e2d33ccac7b78a0f839f5a25e8a548275db0d6e
Ayotola Jinadu <cse080123@gmail.com>
2022-03-26 09:59:27 +0100

    week 8 class

---
+++ CountryAssignment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WandeKevin
+{
+    class CountryAssignment
+    {
+        public static void ReadFile()
+        {
+            string contents = File.ReadAllText(@"C:\temp\test.txt");
+
+            Console.WriteLine(contents);
+        }
+        
+        public static void Main()
+        {
+            ReadFile();
+        }
+    }
+}
